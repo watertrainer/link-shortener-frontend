1d2ea5640bcd1bd4dc9f2f8d7a54135c61c6854c
watertrainer <watertrainer42@gmail.com>
2022-08-15 12:16:54 +0000

    imports for clarity/http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './views/homepage/homepage.component';
@@ -9,6 +11,7 @@ import { StatsComponent } from './views/stats/stats.component';
 import { PrivacyComponent } from './views/privacy/privacy.component';
 import { ImpressumComponent } from './views/impressum/impressum.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,7 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule, FormsModule, AppRoutingModule
+    BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, ClarityModule, BrowserAnimationsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
